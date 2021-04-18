40d8c76fba9f6bd467fd6bc957118449f618de1f
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2021-04-18 16:43:28 +0500

    router is now working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
-  declarations: [	
+  declarations: [		
     AppComponent,
-      HeaderComponent
+      HeaderComponent,
+      LandingComponent
    ],
   imports: [
     BrowserModule,
