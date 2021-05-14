9030a7c2fb6028820d779b9954c679341c14fe0a
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2021-05-15 01:29:03 +0500

    Added placeholder for reviews

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
+import { ReviewsComponent } from './main/reviews/reviews.component';
 
 const routes: Routes = [
+  { path: 'reviews', component: ReviewsComponent },
   { path: '', pathMatch: 'full', component: LandingComponent }
 ];
 
