0e06cde6551177524b8697c8f512ac4d28191099
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2021-05-18 21:22:40 +0500

    Template is ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-// Landing
 import { LandingComponent } from './landing/landing.component';
 import { LandingHeaderComponent } from './landing/header/header.component';
+
+import { ReviewsComponent } from './main/reviews/reviews.component';
 
 @NgModule({
   declarations: [		
     AppComponent,
       LandingComponent,
-      LandingHeaderComponent
+      LandingHeaderComponent,
+      ReviewsComponent
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/main/reviews/reviews.component.ts
+++ src/app/main/reviews/reviews.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-reviews',
   templateUrl: './reviews.component.html',
   styleUrls: ['./reviews.component.scss']
 })
 export class ReviewsComponent {
-  items = ["a","a","a"];
+  Apps = ["a","a","a"];
+  Reviews = [["sadsa"],[],['da','dsa','asdsq']]
 }
