acf62905a3778ece37707b7f109d57726d8c2d80
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2021-05-22 20:10:24 +0500

    making modal in progress

--- src/app/main/reviews/reviews.component.ts
+++ src/app/main/reviews/reviews.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 export class ReviewsComponent {
   Apps = ["a","a","a"];
   Reviews = [["sadsa"],[],['da','dsa','asdsq']]
+
+  openAddAppModal() {
+    let modal = document.querySelector(".add-app-modal");
+    modal.classList.toggle("hidden");
+
+    let link = modal.querySelector("[id=app-link]");
+    console.log(link);
+    console.log(link.textContent);
+    link.textContent = "";
+  }
 }
